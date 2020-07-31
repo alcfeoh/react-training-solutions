3bc41009e7f5760b8c505d23c33fff531e642a03
Marya Doery <mdoery@gmail.com>
2020-07-31 13:43:41 -0400

    Lab DC2: Showing/hiding things - code solution for this lab

---
+++ labDC2/src/LicensePlateComponent.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import './LicensePlateComponent.css';
+const img = require("./assets/sale.png");
+
+class LicensePlateComponent extends Component {
+	buttonClicked() {
+		alert("Plate added to cart");
+	}
+
+	render() {
+		const plate = this.props.plate ? this.props.plate : {};
+		return (
+			<>
+			<h2>{plate.title} {(plate.onSale) ? <img src={img} /> : ''}</h2>
+			<img src={plate.picture} className="img-fluid" />
+			<p>{plate.description}</p>
+			<div>
+			<h2 className="float-left">${plate.price}</h2>
+			<button className="btn btn-primary float-right" role="button" onClick={this.buttonClicked}>
+				{this.props.buttonText}
+			</button>
+			</div>
+			</>
+		);
+	}
+
+};
+
+export default LicensePlateComponent;
