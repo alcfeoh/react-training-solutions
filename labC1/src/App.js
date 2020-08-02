53886117ec15e6ac511fe886aa8b533a836293a8
Marya Doery <mdoery@gmail.com>
2020-08-02 12:16:59 -0400

    Latest solution for lab C1

--- labC1/src/App.js
+++ labC1/src/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
 	constructor() {
 		super();
 		this.name = 'React';
 		this.firstName = 'John';
 	}
+
 	render() {
 		return (
 			<div className="App">
 			<header className="App-header">
-				<p>
-				Hello {this.firstName} {this.name}
-				</p>
 			</header>
+				{/* Add Navigation here */}
+				<main role="main">
+				{/* Add Jumbotron here */}
+				{/* License plates go here */}
+				<h1>
+				Hello {this.firstName} {this.name}
+				</h1>
+				</main>
 			</div>
 		);
 	}
