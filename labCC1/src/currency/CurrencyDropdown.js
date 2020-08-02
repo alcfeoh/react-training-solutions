e69bcaf598ea5ef560c2ae749012b3b1725c12a5
Marya Doery <mdoery@gmail.com>
2020-08-02 15:11:13 -0400

    Add currency dropdown to navbar

--- labCC1/src/currency/CurrencyDropdown.js
+++ labCC1/src/currency/CurrencyDropdown.js
@@ -11,15 +11,15 @@ class CurrencyDropdown extends React.Component {
     }
 	render() {
 		return (
-			<div class="btn-group margin10">
-				<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown"
-					onClick={() => {
-						this.setState({showItems : !this.state.showItems})
-					}}>{this.state.currency}</button>
-				<div class={this.state.showItems? "dropdown-menu show" : "dropdown-menu"}>
-				<a class="dropdown-item" onClick={()=>this.setState({currency: 'USD', showItems : false})}>USD ($)</a>
-				<a class="dropdown-item" onClick={()=>this.setState({currency: 'EUR', showItems : false})}>EUR (€)</a>
-				<a class="dropdown-item" onClick={()=>this.setState({currency: 'GBP', showItems : false})}>GBP (£)</a>
+			<div className="btn-group margin10">
+				<button type="button" className="btn btn-info dropdown-toggle" data-toggle="dropdown"
+onClick={() => {
+this.setState({showItems : !this.state.showItems})
+      }}>{this.state.currency}</button>
+				<div className={this.state.showItems? "dropdown-menu show" : "dropdown-menu"}>
+				<a className="dropdown-item" onClick={()=>this.setState({currency: 'USD', showItems : false})}>USD ($)</a>
+				<a className="dropdown-item" onClick={()=>this.setState({currency: 'EUR', showItems : false})}>EUR (€)</a>
+				<a className="dropdown-item" onClick={()=>this.setState({currency: 'GBP', showItems : false})}>GBP (£)</a>
 				</div>
 			</div>
 		);
--- labCC1/src/navigation/Navigation.js
+++ labCC1/src/navigation/Navigation.js
@@ -1,13 +1,12 @@
 import React, { Fragment } from 'react';
 import CurrencyDropdown from '../currency/CurrencyDropdown';
-import { Link } from "react-router-dom";
 
-class NavTemplate extends React.Component {
+class Navigation extends React.Component {
 	render() {
 		return (
 			<Fragment>
 			<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-				<a className="navbar-brand" href="/">License Plate Store</a>
+				<a className="navbar-brand" href="#">License Plate Store</a>
 				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
@@ -15,17 +14,17 @@ class NavTemplate extends React.Component {
 				<div className="collapse navbar-collapse" id="navbarsExampleDefault">
 					<ul className="navbar-nav mr-auto">
 						<li className="nav-item">
-							<Link to="/" className="nav-link">Home</Link>
+							<a className="nav-link">Home <span className="sr-only">(current)</span></a>
 						</li>
 						<li className="nav-item">
-							<Link to="/cart" className="nav-link">My cart</Link>
+							<a className="nav-link"  >My cart <span className="sr-only"></span></a>
 						</li>
 						<li className="nav-item">
-							<Link to="/checkout" className="nav-link">Checkout</Link>
+							<a className="nav-link" >Checkout <span className="sr-only"></span></a>
 						</li>
 
 					</ul>
-					<CurrencyDropdown setCurrency={this.props.setCurrency} currency={this.props.currency}/>
+					<CurrencyDropdown />
 					<form className="form-inline my-2 my-lg-0">
 						<input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
 						<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
@@ -37,4 +36,4 @@ class NavTemplate extends React.Component {
 	}
 }
 
-export default NavTemplate;
+export default Navigation;
