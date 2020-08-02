119f082b42b7e6396cefe470c7f7496f933fd652
Marya Doery <mdoery@gmail.com>
2020-08-02 14:58:56 -0400

    Refactor code from App.js into LicensePlateService. End state of Lab S1.

--- labS1/src/App.js
+++ labS1/src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import './App.css';
-
-import NavTemplate from './navigation/NavTemplate';
-import JumbotronTemplate from './jumbotron/JumbotronTemplate';
-import LicensePlateService from './LicensePlateService';
+import Navigation from './navigation/Navigation';
+import Jumbotron from './jumbotron/Jumbotron';
+import { LicensePlateComponent } from './license-plate/LicensePlateComponent';
+import {CALIFORNIA_PLATE, LICENSE_PLATES} from "./mock-data";
+import { LicensePlateService } from './LicensePlateService';
 
 class App extends React.Component {
-
-	render() {
-		return (
-			<div className="App">
-			<header className="App-header">
-			</header>
-			{/* Add Navigation here */}
-			<NavTemplate/>
-			<main role="main">
-			{/* Add Jumbotron here */}
-			<JumbotronTemplate title="Welcome to our store" description="Browse our collection of license plates"/>
-			<div className="row" >
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            </header>
+            {/* Add Navigation here */}
+            <Navigation/>
+            <main role="main">
+            {/* Add Jumbotron here */}
+            <Jumbotron title="Welcome to our store" description="Browse our collection of license plates"/>
+            <div className="row" >
 			{/* Replace single license plate with list of license plates here */}
 			<LicensePlateService/>
-			</div>
-			</main>
-			</div>
+            </div>
+            </main>
+            </div>
 		);
 	}
 };
--- labS1/src/LicensePlateService.js
+++ labS1/src/LicensePlateService.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import LicensePlateComponent from './LicensePlateComponent';
+import { LicensePlateComponent } from './license-plate/LicensePlateComponent';
 import {LICENSE_PLATES} from "./mock-data";
-class LicensePlateService extends Component {
+export class LicensePlateService extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -11,16 +11,16 @@ class LicensePlateService extends Component {
     componentDidMount() {
 		this.setState({ data : LICENSE_PLATES })
     }    
-    render() {
-        if (!this.state.data) {
-            return null;
-        }
-        return this.state.data.map((licensePlate, index) => {
-            return <div key={licensePlate._id} className="col-md-4" style={{backgroundColor: (index % 2 === 0) ? '#F5F5F5' : ''}}>
-            <LicensePlateComponent plate={licensePlate} buttonText="Add to cart"/>
-            </div>
-        });
-
-    }
+	render() {
+		if (!this.state.data) {
+			return null;
+		}
+		return this.state.data.map((licensePlate, index) => {
+			return <div key={licensePlate._id} className="col-md-4" style={{backgroundColor: (index % 2 === 0) ? '#F5F5F5' : ''}}>
+			<LicensePlateComponent plate={licensePlate} buttonText="Add to cart"/>
+			</div>
+		});
+  
+	}
+ 
 }
-export default LicensePlateService;
