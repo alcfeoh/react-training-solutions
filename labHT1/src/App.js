f593f66c0dc2c447197a321db7c418e709a48a12
Marya Doery <mdoery@gmail.com>
2020-08-02 15:05:32 -0400

    Minor changes

---
+++ labHT1/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Navigation from './navigation/Navigation';
+import Jumbotron from './jumbotron/Jumbotron';
+import { LicensePlateService } from './LicensePlateService';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            </header>
+            {/* Add Navigation here */}
+            <Navigation/>
+            <main role="main">
+            {/* Add Jumbotron here */}
+            <Jumbotron title="Welcome to our store" description="Browse our collection of license plates"/>
+            <div className="row" >
+			{/* Replace single license plate with list of license plates here */}
+			<LicensePlateService/>
+            </div>
+            </main>
+            </div>
+		);
+	}
+};
+
+export default App;
